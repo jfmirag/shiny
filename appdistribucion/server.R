f79a1c3642739ea656b541cb159b9309c9d9672f
Johan <jfmirag@gmail.com>
2018-02-24 20:56:23 -0500

    cambios en apariencia

--- appdistribucion/server.R
+++ appdistribucion/server.R
@@ -5,7 +5,8 @@ shinyServer(function(input, output) {
    
   output$distPlot <- renderPlot({
 
-    curve(dgamma(x, shape=input$lambda, scale = input$escala, log = F),to=input$maxx,col="blue", type = "l")
+    curve(dgamma(x, shape=input$lambda, scale = input$escala, log = F),to=input$maxx,
+          from=0, ylab="Densidad", las=1, lwd=3, col="red")
     
     
   })
--- appdistribucion/ui.R
+++ appdistribucion/ui.R
@@ -7,9 +7,6 @@ shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      withMathJax(),
-       helpText('An irrational number \\(\\sqrt{2}\\)
-           and a fraction $$1-\\frac{1}{2}$$'),
        numericInput("lambda", "l", value=1.5, min=0, max=50, step=0.1),
        numericInput("escala", "k", value=1, min=0, max=50, step=0.1),
        sliderInput("maxx", "Maximo valor de x:", min = 0, max = 150, value = 10, step = 1)
